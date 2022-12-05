9bf8cf7a5c48450c83bf20fdedc64a412b4322ec
EmanuelPrgic <emanuel.prgic@fpmoz.sum.ba>
2022-12-06 00:13:47 +0100

    treci commit

--- index.php
+++ index.php
@@ -1,3 +1,123 @@
-<?php
-	echo "<h1> Pocetak Projekta"
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Scrolling Nav - Start Bootstrap Template</title>
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="css/styles.css" rel="stylesheet" />
+</head>
+<body id="page-top">
+<!-- Navigation-->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+    <div class="container px-4">
+        <a class="navbar-brand" href="#page-top">Dating App</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link" href="#about">O nama</a></li>
+                <li class="nav-item"><a class="nav-link" href="#services">Usluge</a></li>
+                <li class="nav-item"><a class="nav-link" href="#contact">Kontakt</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<!-- Header-->
+<header class="bg-primary bg-gradient text-white">
+    <div class="container px-4 text-center">
+        <h1 class="fw-bolder">Dating App</h1>
+        <p class="lead">Novi twist na Dating App, napravili Emanuel i Stjepan Prgić</p>
+        <a class="btn btn-lg btn-light" href="/createAccount">Napravi Račun</a>
+    </div>
+</header>
+<!-- About section-->
+<section id="about">
+    <div class="container px-4">
+        <div class="row gx-4 justify-content-center">
+            <div class="col-lg-8">
+                <h2>O ovoj stranici</h2>
+                <p class="lead">Ovo je početna stranica koja će objasniti nas cilj i namjenu ovoga App-a.</p>
+                <ul>
+                    <li>Svrha ove aplikacije je olakšavanje korisnicima da nađu i upoznaju partnera online uslugama koje druge aplikacije nemaju.</li>
+                    <li>Glavna usluga koju ćemo pružati je mogućnost da korisnik pošalje svoj profil na review kod Dating Experta koji će dati feedback.</li>
+                    <li>S tim feedback-om korisnik može poboljšati svoj profil kako bi bio što više privlačniji.</li>
+                    <li>Jedni od primjera su slike na profilu, tj. ako je korisnik izabrao loše slike te loš opis.</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Services section-->
+<section class="bg-light" id="services">
+    <div class="container px-4">
+        <div class="row gx-4 justify-content-center">
+            <div class="col-lg-8">
+                <h2>Usluge koje mi pružamo</h2>
+                <ul>
+                    <li>Brza i responzivna aplikacija</li>
+                    <li>Brza tehnička podrška</li>
+                    <li>Kvalificirani experti za Dating svijet</li>
+                    <li>Veliki izbor mogućnosti</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Contact section-->
+<section id="contact">
+    <div class="container px-4">
+        <div class="row gx-4 justify-content-center">
+            <h2>Kontakt</h2>
+            <p class="lead">Ovu aplikaciju su napravili Emanuel i Stjepan Prgić, te su naši kontakti navedeni ispod.</p>
+        </div>
+        <div class="row gx-4 justify-content-center">
+            <div class="col-6">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="pictures/emanuelprgic.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Emanuel Prgić</h5>
+                        <p class="card-text">Student FPMOZ, Informatika, 3. godina</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">20.12.2001.</li>
+                        <li class="list-group-item">M</li>
+                        <li class="list-group-item">github.com/EmanuelPrgic</li>
+                    </ul>
+                    <div class="card-body">
+                        <a href="emanuelprgic" class="card-link">Pokaži Profil</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 justify-content-center">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="pictures/stjepanprgic.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Stjepan Prgić</h5>
+                        <p class="card-text">Student FPMOZ, Informatika, 3. godina</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">21.10.1998</li>
+                        <li class="list-group-item">M</li>
+                        <li class="list-group-item">github.com/StjepanPrgic</li>
+                    </ul>
+                    <div class="card-body">
+                        <a href="stjepanprgic" class="card-link">Pokaži profil</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Footer-->
+<footer class="py-5 bg-dark">
+    <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; Dating App 2022</p></div>
+</footer>
+<!-- Bootstrap core JS-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+<!-- Core theme JS-->
+<script src="js/scripts.js"></script>
+</body>
+</html>
